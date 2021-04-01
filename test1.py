7c9799e1524425e5071b1a3faea5e8d09b46c266
zclh <d727953119@gmail.com>
2021-04-01 22:48:55 +0800

    通过读取txt文件的get配置进行等价类和边界值测试

--- test1.py
+++ test1.py
@@ -1,16 +1,24 @@
 #-*- coding: utf-8 -*-
 import requests
 import json
+import os
 
-url = 'http://api.k780.com:88/?app=phone.get&phone=13093452121&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json'
-parms={}
-r = requests.get(url)
-assert r.status_code == 200,"连接出错"
-d = json.loads(r.text)
-if d['success'] == "1":
-    print("检查通过")
-    with open("test1.json", 'w', encoding='utf-8')as f:
-        json.dump(json.loads(r.text), f,ensure_ascii=False)
-        f.close()
-else:
-    print("检查不通过")
+file1 = open("test.txt",'r')
+
+while True:
+    lines = file1.readline()
+    url = lines
+    parms={}
+    r = requests.get(url)
+    assert r.status_code == 200,"连接出错"
+    d = json.loads(r.text)
+    if d['success'] == "1":
+        print("检查通过")
+        with open("test1.json", 'w', encoding='utf-8')as f:
+            json.dump(json.loads(r.text), f,ensure_ascii=False)
+            f.close()
+    else:
+        print("检查不通过")
+    if not lines:
+        break
+file1.close()   
